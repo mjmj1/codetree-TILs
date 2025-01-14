ef5b3007f71b4f297e2443e6766cea1dc7461832
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-01-14 17:43:41 +0900

    Title: 함수를 이용한 소수 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250114/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 250114/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int a, b;
 
 bool is_prime(int n) {
-    if(n < 3)
+    if(n < 2)
     {
         return false;
     }
-    
+
     for(int i = 2; i <= sqrt(n); i++)
     {
         if(n % i == 0)
