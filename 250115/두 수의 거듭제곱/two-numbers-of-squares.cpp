4f6bd7e30c3cea510ce0436a93e9b71ed9875b15
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-01-15 09:46:58 +0900

    Title: 두 수의 거듭제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250115/두 수의 거듭제곱/two-numbers-of-squares.cpp
+++ 250115/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int a, b;
 
+int get_power(int a, int b)
+{
+    return pow(a, b);
+}
+
 int main() {
     cin >> a >> b;
 
-    printf("%d", pow(a, b));
+    printf("%d", get_power(a, b));
 
     return 0;
 }
