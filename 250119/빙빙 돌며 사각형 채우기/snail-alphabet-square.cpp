20c5055170f6dfcd1ad885203f1afafdac58e996
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-01-19 17:21:21 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+char arr[100][100];
+
+int dir[4][2] =
+{
+    { 0, 1 },
+    { 1, 0 },
+    { 0, -1 },
+    { -1, 0 },
+};
+
+bool in_range(int x, int y)
+{
+    return (m > x && x >= 0) && (n > y && y >= 0);
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    char cnt = 1;
+    int nx = 0, ny = 0;
+    int dx = 0, dy = 0;
+    int rot = 0;
+
+    arr[0][0] = 65;
+
+    for (int i = 0; i < n * m - 1; i++)
+    {
+        dx = nx + dir[rot][1], dy = ny + dir[rot][0];
+
+        if (!in_range(dx, dy) || arr[dy][dx] != 0)
+        {
+            rot = (rot + 1) % 4;
+        }
+
+        nx += dir[rot][1], ny += dir[rot][0];
+
+        arr[ny][nx] = 65 + ((cnt++) % 26);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%c ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
