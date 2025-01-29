7065f6b4da62a80bbcb7bf2ec4c517af7208e565
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-01-29 15:40:12 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int n;
+int nums[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", nums[i]); 
+    }
+
+    printf("\n");
+
+    sort(nums, nums + n, greater<int>());
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", nums[i]); 
+    }
+
+    return 0;
+}
