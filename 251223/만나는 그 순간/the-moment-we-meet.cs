b585bfff1e5f3ab9ae051a29536e7175cf2d8a07
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-23 15:35:41 +0900

    Title: 만나는 그 순간, Time: 62ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251223/만나는 그 순간/the-moment-we-meet.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using System.Text;
+
+public class Codetree
+{
+    public static void Main()
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        StringBuilder sb = new StringBuilder();
+
+        var firstLine = sr.ReadLine().Split(' ');
+        int n = int.Parse(firstLine[0]);
+        int m = int.Parse(firstLine[1]);
+
+        int[] posA = new int[1000];
+        int[] posB = new int[1000];
+        int meet = -1;
+
+        int cur = 0;
+        int offset = 0;
+
+        // A의 이동 정보 입력
+        for (int i = 0; i < n; i++)
+        {
+            var inputs = sr.ReadLine().Split(' ');
+            string dir = inputs[0];
+            int time = int.Parse(inputs[1]);
+
+            int sign = 0;
+            if (dir == "L") sign = -1;
+            else if (dir == "R") sign = 1;
+
+            for(var j = offset; j < offset + time; j++)
+            {
+                cur += sign;
+                posA[j] = cur;
+            }
+
+            offset = offset + time;
+        }
+
+        cur = 0;
+        offset = 0;
+
+        // B의 이동 정보 입력
+        for (int i = 0; i < m; i++)
+        {
+            var inputs = sr.ReadLine().Split(' ');
+            string dir = inputs[0];
+            int time = int.Parse(inputs[1]);
+
+            int sign = 0;
+            if (dir == "L") sign = -1;
+            else if (dir == "R") sign = 1;
+
+            for(var j = offset; j < offset + time; j++)
+            {
+                cur += sign;
+                posB[j] = cur;
+            }
+
+            offset = offset + time;
+        }
+
+        for(var i = 0; i < posA.Length; i++)
+        {
+            if(posA[i] == posB[i])
+            {
+                meet = i + 1;
+
+                break;
+            }
+        }
+
+        // 4. 결과 출력
+        sb.Append($"{meet}"); 
+        sw.Write(sb);
+
+        sr.Close();
+        sw.Close();
+    }
+}
