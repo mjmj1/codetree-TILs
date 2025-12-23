c526c1867d8a7874321c517fdc436c156da2a0ac
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-23 10:28:42 +0900

    Title: 연속되는 수 4, Time: 62ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251223/연속되는 수 4/continuous-number4.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Text;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        StringBuilder sb = new StringBuilder();
+
+        int prev = 1001;
+        int cnt = 1;
+        int max = 0;
+
+        int n = int.Parse(sr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var x = int.Parse(sr.ReadLine());
+
+            if(prev < x)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt = 1;
+            }
+
+            if(cnt > max)
+            {
+                max = cnt;
+            }
+
+            prev = x;
+        }
+
+        sb.Append($"{max}");
+
+        sw.Write(sb);
+        sw.Close();
+        sr.Close();
+    }
+}
