f9f1537230d8ef8b5e44aeba265c1fce9d29c350
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-23 10:23:22 +0900

    Title: 연속되는 수 3, Time: 65ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251223/연속되는 수 3/continuous-number3.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Text;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        StringBuilder sb = new StringBuilder();
+
+        int prev = 0;
+        int max = 0;
+        int cnt = 1;
+
+        int n = int.Parse(sr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var x = int.Parse(sr.ReadLine());
+
+            if(x > 0 && prev > 0)
+            {
+                cnt++;
+            }
+            else if(x < 0 && prev < 0)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt = 1;
+            }
+
+            if(cnt > max)
+            {
+                max = cnt;
+            }
+
+            prev = x;
+        }
+
+        sb.Append($"{max}");
+
+        // 6. 최종 출력 및 닫기
+        sw.Write(sb);
+        sw.Close();
+        sr.Close();
+    }
+}
