59cc207c949d894f976a8c56c3337ecd7ff580fe
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-24 11:32:12 +0900

    Title: 좌우로 움직이는 로봇, Time: 65ms, Memory: 8MB, Status: Wrong Answer - Codetree

---
+++ 251224/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cs
@@ -0,0 +1,77 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Codetree
+{
+    public static void Main()
+    {
+        var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        var sb = new StringBuilder();
+
+        var inputs = sr.ReadLine().Split(' ');
+        var n = int.Parse(inputs[0]);
+        var m = int.Parse(inputs[1]);
+
+        var posA = new List<int>();
+        var posB = new List<int>();
+
+        for(var i = 0; i < n; i++)
+        {
+            var line = sr.ReadLine().Split(' ');
+            var x = int.Parse(line[0]);
+            var dir = line[1];
+            
+            var cur = posA.LastOrDefault();
+            for(var j = 0; j < x; j++)
+            {
+                if(dir == "L") cur--;
+                else cur++;
+
+                posA.Add(cur);
+            }
+        }
+
+        for(var i = 0; i < m; i++)
+        {
+            var line = sr.ReadLine().Split(' ');
+            var x = int.Parse(line[0]);
+            var dir = line[1];
+
+            var cur = posB.LastOrDefault();
+            for(var j = 0; j < x; j++)
+            {
+                if(dir == "L") cur--;
+                else cur++;
+
+                posB.Add(cur);
+            }
+        }
+
+        int max = Math.Max(posA.Count, posB.Count);
+        int cnt = 0;
+
+        for(var i = 0; i < max; i++)
+        {
+            var a = Math.Min(i, posA.Count - 1);
+            var b = Math.Min(i, posB.Count - 1);
+
+            if(posA[a] == posB[b])
+            {
+                if(posA[a-1] != posB[b-1])
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        sb.Append($"{cnt}");
+        sw.Write(sb);
+
+        sr.Close();
+        sw.Close();
+    }
+}
