54ea3715dd81b23c492be127e1478d7202ac2c49
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-24 17:27:09 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 71ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251224/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Codetree
+{
+    public static void Main()
+    {
+        var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        var sb = new StringBuilder();
+
+        var inputs = sr.ReadLine().Split(' ');
+        var n = int.Parse(inputs[0]);
+        var k = int.Parse(inputs[1]);
+        var p = int.Parse(inputs[2]);
+        var t = int.Parse(inputs[3]);
+
+        var infected = new int[n];
+        var infectedCnt = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if(i != p - 1) continue;
+
+            infected[i] = 1;
+            infectedCnt[i] = k;
+        }
+
+        var times = new SortedList<int, (int, int)>();
+
+        for (var i = 0; i < t; i++)
+        {
+            var line = sr.ReadLine().Split(' ');
+
+            var t_ = int.Parse(line[0]);
+            var x = int.Parse(line[1]) - 1;
+            var y = int.Parse(line[2]) - 1;
+            
+            times.Add(t_, (x, y));
+        }
+
+        foreach(var kvp in times)
+        {
+            var value = kvp.Value;
+
+            var x = value.Item1;
+            var y = value.Item2;
+
+            // sw.WriteLine($"{x}:{infected[x]} | {y}:{infected[y]}");
+
+            if (infected[x] > 0)
+            {
+                if(infectedCnt[x] == 0) continue;
+            
+                infectedCnt[x]--;
+
+                if(infected[y] == 1) continue;
+                
+                infected[y] = 1;
+                infectedCnt[y] = k;
+
+            }
+            else if (infected[y] > 0)
+            {
+                if(infectedCnt[y] == 0) continue;
+            
+                infectedCnt[y]--;
+
+                if(infected[x] == 1) continue;
+
+                infected[x] = 1;
+                infectedCnt[x] = k;
+            }
+        }
+        
+        for (int i = 0; i < infected.Length; i++)
+        {
+            sb.Append($"{infected[i]}");
+        }
+
+        sw.Write(sb);
+
+        sr.Close();
+        sw.Close();
+    }
+}
