19b7f8218d6739d9131fb90eb6dcb3a4a837c9e3
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-26 13:16:21 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 64ms, Memory: 6MB, Status: Wrong Answer - Codetree

---
+++ 251226/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Codetree
+{
+    public static void Main()
+    {
+        var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        var sb = new StringBuilder();
+
+        var dx = new int[4] { 1, 0, -1, 0 };
+        var dy = new int[4] { 0, -1, 0, 1 };
+
+        var n = int.Parse(sr.ReadLine());
+
+        var grid = new int[n,n];
+        var center = n / 2;
+        var curX = center;
+        var curY = center;
+        var dir = 0;
+        var idx = 1;
+        var r = 1;
+
+        grid[center, center] = idx++;
+
+        while(idx < n * n)
+        {
+            for(var j = 0; j < 2; j++)
+            {
+                for(var k = 0; k < r; k++)
+                {
+                    curX += dx[dir];
+                    curY += dy[dir];
+
+                    grid[curY, curX] = idx++;
+
+                    if(idx > n * n) break;
+                }
+
+                dir = (dir + 1) % 4;
+            }
+
+            r++;
+        }
+        
+        for(var i = 0; i < n; i++)
+        {
+            for(var j = 0; j < n; j++)
+            {
+                sb.Append($"{grid[i, j]}");
+            }
+
+            sb.Append("\n");
+        }
+        
+        sw.Write(sb);
+
+        sr.Close();
+        sw.Close();
+    }
+}
