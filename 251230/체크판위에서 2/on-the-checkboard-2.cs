372735006452c3c212caf9bbc90387d0068db1e4
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-30 14:05:10 +0900

    Title: 체크판위에서 2, Time: 65ms, Memory: 5MB, Status: Wrong Answer - Codetree

--- 251230/체크판위에서 2/on-the-checkboard-2.cs
+++ 251230/체크판위에서 2/on-the-checkboard-2.cs
@@ -16,12 +16,49 @@ public class Codetree
         var r = int.Parse(inputs[0]);
         var c = int.Parse(inputs[1]);
 
-        var arr = new int[r,c];
+        var arr = new char[r,c];
         for(var i = 0; i < r; i++)
         {
-            arr[i] = sr.ReadLine().Split(' ').Select(int.Parse).ToArray()'
+            var line = sr.ReadLine().Split(' ');
+            for(var j = 0; j < c; j++)
+            {
+                var ch = line[j][0];
+                arr[i,j] = ch;
+            }
+        }
+
+        var result = 1;
+        var curR = 0;
+        var curC = 0;
+        var cur = 0;
+        var start = arr[curR, curC];
+
+        for(var k = 1; k < r - 1; k++)
+        {
+            cur = 0;
+
+            for(var i = curR + 1; i < r - 1; i++)
+            {
+                for(var j = curC + 1; j < c - 1; j++)
+                {
+                    if(start != arr[i, j])
+                    {
+                        curR = i;
+                        curC = j;
+                        cur++;
+                    }
+                }
+
+                if(cur > 0)
+                {
+                    start = arr[curR, curC];
+                    result *= cur;
+                    break;
+                }
+            }
         }
         
+        sb.Append($"{result}");
         sw.Write(sb);
 
         sr.Close();
