1fe7d7021103a92e5e6adca847206847a35e252d
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-30 16:40:33 +0900

    Title: 이상한 진수 2, Time: 59ms, Memory: 5MB, Status: Wrong Answer - Codetree

--- 251230/이상한 진수 2/awkward-digits-2.cs
+++ 251230/이상한 진수 2/awkward-digits-2.cs
@@ -15,7 +15,19 @@ public class Codetree
         var n = sr.ReadLine();
         var chars = n.ToCharArray();
         
-        for(var i = 0; i < n.Length; i++)
+        if(n.Length == 1)
+        {
+            if(n[0] == '0')
+            {
+                chars[0] = '1';
+            }
+            else
+            {
+                chars[0] = '0';
+            }
+        }
+
+        for(var i = 1; i < n.Length; i++)
         {
             if(n[i] == '0')
             {
