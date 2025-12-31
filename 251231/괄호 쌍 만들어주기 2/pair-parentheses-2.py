e606ed6e9e923105731fd4120c8e76c2edb8f426
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2025-12-31 17:06:28 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251231/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,10 @@
+A = input()
+
+cnt = 0
+for i in range(len(A) - 2):
+    if A[i] == '(' and A[i + 1] =='(':
+        for j in range(i + 1, len(A) - 1):
+            if A[j] == ')' and A[j + 1] ==')':
+                cnt+=1
+
+print(cnt)
